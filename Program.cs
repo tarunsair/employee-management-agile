88f8bd73d7f97320b83628dc29981f6f0b493d56
tarunsair <tarunsaiece11@gmail.com>
2026-03-11 18:49:38 -0500

    updated Program file

--- Program.cs
+++ Program.cs
@@ -64,6 +64,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 // app.MapControllerRoute();
 app.MapControllerRoute(
